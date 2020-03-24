d5b33776d1f50c3fc93d54e99b5cf43585a1cd82
renlok87 <renlok87@gmail.com>
2020-03-24 13:00:00 +0300

    add controllers

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 
+const db = require('./core/db');
+const {
+    patientValidation,
+    appointmentValidation
+} = require('./utils/validations');
+const { PatientCtrl, AppointmentCtrl } = require('./controllers');
+
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get('/patients', PatientCtrl.all);
+app.post('/patients', patientValidation.create, PatientCtrl.create);
 
+app.get('/appointments', AppointmentCtrl.all);
+app.post('/appointments', appointmentValidation.create, AppointmentCtrl.create);
 
 app.listen(6666, function(err) {
     if (err) {
